dc982d527cd0a6086ffeac92968df3427b9909ee
Rob <rdaly1490@gmail.com>
2016-12-26 14:13:40 -0500

    play and pause work with button and on video player click

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+	if (video.paused) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+function updatePlayButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+toggle.addEventListener('click', togglePlay);
+
+
+
